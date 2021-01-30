496856ce515a5f02e20aba9b15d4bf5318641052
brianv-17 <69707282+brianv-17@users.noreply.github.com>
2021-01-29 19:45:15 -0500

    jan29

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -3,6 +3,7 @@ import { Link } from '@reach/router';
 export default () => {
     return (
         <div>
+            <h2>Sharing a Little About me</h2>
             <p>this is a test</p>
             <Link to = "/portfolio">Home Page</Link>
         </div>
