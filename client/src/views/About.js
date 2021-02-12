fcce3ec9f0e70f8cdaad733689ebe304b39cf7c2
brianv-17 <69707282+brianv-17@users.noreply.github.com>
2021-02-11 19:03:11 -0500

    feb11

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -5,9 +5,9 @@ export default () => {
     return (
         <div>
             <div>
-                <h2>Sharing a Little About me</h2>
-                <p>Born in Detroit, Michigan I lived most of my youth (ages one to eight) in the outskirts of Tepatitlan, Jalisco, Mexico. I came to a small mill town in South Carolina named Georgetown at the age of 8 with no knowledge of the english language. Throughout my adolecsence I made Georgetown my home </p>
-                <p>this is a test</p>
+                <h1>Sharing a Little About me</h1>
+                <h2>Personal life </h2>
+                <p>Born to an immigrant family in Detroit, Michigan I lived most of my youth (ages one to eight) in the outskirts of Tepatitlan, Jalisco, Mexico. I came to a small mill town in South Carolina named Georgetown at the age of 8 with no knowledge of the english language. Throughout my adolecsence I made Georgetown my home </p>
                 <p>Some of the activities that I dabble aside from programming</p>
                 <ul>
                     <li>I play video games and live stream</li>
